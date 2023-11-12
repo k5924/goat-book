0edd0babdb7e11630a1cc4296c480f2add965908
Kamran Bhatti <k.bhatti5384@yahoo.co.uk>
2023-11-12 14:35:21 +0000

    Subject: create a helper method to enter items into the todo list

--- functional_tests.py
+++ functional_tests.py
@@ -17,6 +17,17 @@ class NewVisitorTest(unittest.TestCase):
         rows = table.find_elements(By.TAG_NAME, "tr")
         self.assertIn(row_text, [row.text for row in rows])
 
+    def create_entry_in_list(self, entry_text):
+        # She is invited to enter a to-do item
+        inputbox = self.browser.find_element(By.ID, "id_new_item")
+        self.assertEqual(inputbox.get_attribute("placeholder"), "Enter a to-do item")
+
+        inputbox.send_keys(entry_text)
+
+        # When she hits enter, the page updates, and now the page lists
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
+
     def test_can_start_a_todo_list(self):
         # Edith has heard about a cool new online to-do app.
         # She goes to check out its homepage
@@ -27,28 +38,17 @@ class NewVisitorTest(unittest.TestCase):
         header_text = self.browser.find_element(By.TAG_NAME, "h1").text
         self.assertIn("To-Do", header_text)
 
-        # She is invited to enter a to-do item straight away
-        inputbox = self.browser.find_element(By.ID, "id_new_item")
-
-        self.assertEqual(inputbox.get_attribute("placeholder"), "Enter a to-do item")
-
         # She types "Buy peacock feathers" into a text box
         # (Edith's hobby is typing fly-fishing lures)
-        inputbox.send_keys("Buy peacock feathers")
+        self.create_entry_in_list("Buy peacock feathers")
 
         # When she hits enter, the page updates, and now the page lists
         # 1: "Buy peacock feathers" as an item in a to-do list
-        inputbox.send_keys(Keys.ENTER)
-        time.sleep(1)
-
         self.check_for_row_in_list_table("1: Buy peacock feathers")
 
         # There is still a text box inviting her to add another item.
         # She enters "Use peacock feathers to make a fly" (Edith is very methodical)
-        inputbox = self.browser.find_element(By.ID, "id_new_item")
-        inputbox.send_keys("Use peacock feathers to make a fly")
-        inputbox.send_keys(Keys.ENTER)
-        time.sleep(1)
+        self.create_entry_in_list("Use peacock feathers to make a fly")
 
         # The page updates again, and now shows both items on her list
         self.check_for_row_in_list_table("1: Buy peacock feathers")
